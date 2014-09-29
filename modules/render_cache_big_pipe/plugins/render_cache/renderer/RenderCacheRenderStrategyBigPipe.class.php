65ee5a64e8deff523af658c0360ef0559d2c345e
Fabian Franz <github@fabian-franz.de>
2014-09-29 10:10:47 -0700

    Issue #2346897: Add first implementation of render_cache big pipe.

--- modules/render_cache_big_pipe/plugins/render_cache/renderer/RenderCacheRenderStrategyBigPipe.class.php
+++ modules/render_cache_big_pipe/plugins/render_cache/renderer/RenderCacheRenderStrategyBigPipe.class.php
@@ -17,15 +17,40 @@ class RenderCacheRenderStrategyBigPipe extends RenderCacheRenderStrategyBase {
   public function render(array $args) {
     $placeholders = array();
     foreach ($args as $placeholder => $ph_object) {
-      $placeholders[$placeholder] = array();
-
       // @todo Replace with a nice loading theme.
-      $placeholders[$placeholder]['#markup'] = $placeholder;
+      $placeholders[$placeholder] = array();
+      $id = drupal_html_id('render-cache-big-pipe-' . $ph_object['type'] . '-' . $ph_object['id']);
+      $placeholders[$placeholder]['#markup'] = '<div id="' . $id . '"></div>';
 
       // Store the data for later usage.
-      static::$placeholders[$placeholder] = $ph_object;
+      static::$placeholders[$id] = $ph_object;
     }
 
     return $placeholders;
+  }
+
+  public function renderPlaceholder($placeholder, $ph_object) {
+    $rcc = render_cache_get_controller($ph_object['type']);
+    $rcc->setContext($ph_object['context']);
+    $objects = array(
+      $ph_object['id'] => $ph_object['object'],
+    );
+    $build = $rcc->viewPlaceholders($objects);
+    $output = drupal_render($build);
+
+    $html = drupal_json_encode($output);
+
+    // @todo Add helper function.
+    $markup = <<<EOF
+<script type="text/javascript">
+var element = document.getElementById("$placeholder");
+var newElement = document.createElement("span");
+newElement.innerHTML = $html;
+var newChild = newElement.firstChild;
+element.parentNode.replaceChild(newChild, element);
+</script>;
+EOF;
+
+    return $markup;
   }
 }
