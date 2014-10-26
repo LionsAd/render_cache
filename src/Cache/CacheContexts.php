36a98ce2fb41216b741becfd05e674ea575771d0
Fabian Franz <github@fabian-franz.de>
2014-10-26 18:58:56 +0100

    Issue #2351729: Add render_cache version of CacheContexts.

---
+++ src/Cache/CacheContexts.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * @file
+ * Contains \Drupal\render_cache\Cache\CacheContexts
+ */
+
+namespace Drupal\render_cache\Cache;
+
+use Drupal\render_cache\DependencyInjection\Container;
+use Drupal\render_cache\DependencyInjection\ContainerInterface;
+
+use Drupal\Core\Cache\CacheContexts as DrupalCacheContexts;
+
+/**
+ * Defines the CacheContexts service.
+ *
+ * Provides CacheContexts service using the render_cache container.
+ *
+ * Note: The original Drupal 8 CacheContexts service uses the Symfony
+ *       ContainerInterface, which is not available here. So we over-
+ *       write the constructor to enable us to use the functionality of the
+ *       stock file.
+ *
+ * @ingroup cache
+ */
+class CacheContexts extends DrupalCacheContexts {
+
+  /**
+   * The service container.
+   *
+   * @var \Drupal\render_cache\DependencyInjection\ContainerInterface
+   */
+  protected $container;
+
+ /**
+   * Constructs a CacheContexts object.
+   *
+   * @param \Drupal\render_cache\DependencyInjection\ContainerInterface $container
+   *   The current service container.
+   * @param array $contexts
+   *   An array of key-value pairs, where the keys are service names (which also
+   *   serve as the corresponding cache context token) and the values are the
+   *   cache context labels.
+   */
+  public function __construct(ContainerInterface $container, array $contexts) {
+    $this->container = $container;
+    $this->contexts = $contexts;
+  }
+}
