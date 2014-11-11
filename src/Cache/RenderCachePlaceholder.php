d6c28d820edebf39f152dc60b4012d5c7a5cc173
Fabian Franz <github@fabian-franz.de>
2014-11-11 04:27:40 -0800

    Issue #2365469: Make RenderCachePlaceholder testable; improve _load functionality.

--- src/Cache/RenderCachePlaceholder.php
+++ src/Cache/RenderCachePlaceholder.php
@@ -17,16 +17,16 @@ class RenderCachePlaceholder implements RenderCachePlaceholderInterface {
    * {@inheritdoc}
    */
   public static function getPlaceholder($function, array $args = array(), $multiple = FALSE) {
-    // Add namespace to the front.
-    $callback = "\\Drupal\\render_cache\\Cache\\";
-    $callback .= $multiple ? 'RenderCachePlaceholder::postRenderCacheMultiCallback' : 'RenderCachePlaceholder::postRenderCacheCallback';
+    // Add the classname to the front.
+    $callback = get_called_class();
+    $callback .= $multiple ? '::postRenderCacheMultiCallback' : '::postRenderCacheCallback';
 
     $context = array(
       'function' => $function,
       'args' => $args,
     );
 
-    $placeholder = drupal_render_cache_generate_placeholder($context['function'], $context);
+    $placeholder = static::generatePlaceholder($context['function'], $context);
 
     $context = array($context);
     if ($multiple) {
@@ -55,21 +55,21 @@ class RenderCachePlaceholder implements RenderCachePlaceholderInterface {
     $function = $contexts[0]['function'];
     $args = array();
     foreach ($contexts as $context) {
-      $placeholder = drupal_render_cache_generate_placeholder($context['function'], $context);
+      $placeholder = static::generatePlaceholder($context['function'], $context);
 
       // Check if the placeholder is present at all.
       if (strpos($element['#markup'], $placeholder) === FALSE) {
         continue;
       }
 
-      $args[$placeholder] = $context['args'];
+      $args[$placeholder] = static::loadPlaceholderFunctionArgs($context);
     }
 
     // This expects an array keyed by placeholder with the build as value.
     $placeholders = call_user_func($function, $args);
 
     foreach ($placeholders as $placeholder => $new_element) {
-      $markup = drupal_render($new_element);
+      $markup = static::drupalRender($new_element);
       $element['#markup'] = str_replace($placeholder, $markup, $element['#markup']);
     }
 
@@ -81,7 +81,7 @@ class RenderCachePlaceholder implements RenderCachePlaceholderInterface {
    */
   public static function postRenderCacheCallback(array $element, array $context) {
 
-    $placeholder = drupal_render_cache_generate_placeholder($context['function'], $context);
+    $placeholder = static::generatePlaceholder($context['function'], $context);
 
     // Check if the placeholder is present at all.
     if (strpos($element['#markup'], $placeholder) === FALSE) {
@@ -92,7 +92,7 @@ class RenderCachePlaceholder implements RenderCachePlaceholderInterface {
     $args = static::loadPlaceholderFunctionArgs($context);
     $new_element = call_user_func_array($function, $args);
 
-    $markup = RenderCache::drupalRender($new_element);
+    $markup = static::drupalRender($new_element);
     $element['#markup'] = str_replace($placeholder, $markup, $element['#markup']);
 
     return $element;
@@ -106,15 +106,59 @@ class RenderCachePlaceholder implements RenderCachePlaceholderInterface {
 
     foreach ($context['args'] as $key => $arg) {
       // In case a dynamic argument has been passed, load it with the loader.
-      if (preg_match('/^%(|' . DRUPAL_PHP_FUNCTION_PATTERN . ')$/', $key, $matches)) {
-        if (!empty($matches[1]) && function_exists($matches[1] . '_load')) {
-          $loader_function = $matches[1] . '_load';
-          $arg = $loader_function($arg);
+      if (strpos($key, '%') !== 0) {
+        $args[] = $arg;
+        continue;
+      }
+
+      $prefix = substr($key, 1);
+      $loader_functions = array(
+        $prefix . '_load',
+        $prefix . 'Load',
+      );
+      foreach ($loader_functions as $loader_function) {
+        if (is_callable($loader_function)) {
+          $arg = call_user_func($loader_function, $arg);
         }
       }
       $args[] = $arg;
     }
 
     return $args;
+  }
+
+  /**
+   * Generates a render cache placeholder.
+   *
+   * @param string $callback
+   *   The #post_render_cache callback that will replace the placeholder with its
+   *   eventual markup.
+   * @param array $context
+   *   An array providing context for the #post_render_cache callback. This array
+   *   will be altered to provide a 'token' key/value pair, if not already
+   *   provided, to uniquely identify the generated placeholder.
+   *
+   * @return string
+   *   The generated placeholder HTML.
+   *
+   * @codeCoverageIgnore
+   */
+  protected static function generatePlaceholder($callback, array &$context) {
+    return drupal_render_cache_generate_placeholder($callback, $context);
+  }
+
+  /**
+   * Overrides drupal_render().
+   *
+   * @param array $elements
+   *   The elements to render.
+   *
+   * @return string
+   *   The rendered HTML string.
+   *
+   * @codeCoverageIgnore
+   */
+  protected static function drupalRender(array &$elements) {
+    return RenderCache::drupalRender($elements);
   }
 }
